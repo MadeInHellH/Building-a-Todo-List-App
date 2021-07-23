b153f9f5461a33683ddca1d41fe7471e9b1587eb
marcelo <madeinhellh@gmail.com>
2021-07-23 02:07:45 -0800

    hasta el minuto 19

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,19 +1,47 @@
 import React, { useState } from 'react';
+import { connect } from "react-redux";
+import { addTodos }from "../redux/reducer";
 
-const Todos = () => {
+const mapStateToProps = (state) => {
+    return {
+        todos: state,
+    };
+};
 
-    const [todo,setTodo] = useState("")
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addTodo: (obj) => dispatch(addTodos(obj))
+    }
+}
+
+const Todos = (props) => {
+ const [todo,setTodo] = useState("")
 
     const handleChange = (e) =>{
         setTodo(e.target.value);
     };
-    console.log("todo text", todo);
+    console.log("props from store", props);  
+    return (
+        <div className="addTodos">
+    <input 
+    type="text" 
+    onChange={ (e) => handleChange (e) } 
+    className="todo-input"
+    />  
 
-    return <div className="addTodos">
-    <input type="text" onChange={ (e) => handleChange (e) } className="todo-input"/>  
-
-    <button className="add-btn">Add</button>  
+    <button className="add-btn"
+            onClick={() =>
+            props.addTodo({
+                id: Math.floor(Math.random() * 1000),
+                item: todo,
+                complete: false,
+            })
+            }
+            >
+                Add
+            </button>  
         </div>
+    );
 };
-
-export default Todos;
+// we can use connect method to connect this component with redux store
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
